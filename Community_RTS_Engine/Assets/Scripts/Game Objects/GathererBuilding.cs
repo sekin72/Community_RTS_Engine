f439d7f5a0e4868d08f5d2c864d632fa8f2d5f65
batikanhyt <batikanhyt@gmail.com>
2018-07-24 05:20:20 +0300

    kingdom bitti galiba

--- Community_RTS_Engine/Assets/Scripts/Game Objects/GathererBuilding.cs
+++ Community_RTS_Engine/Assets/Scripts/Game Objects/GathererBuilding.cs
@@ -5,77 +5,182 @@ using System.Text;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 namespace Assets.Scripts.Game_Objects
 {
     class GathererBuilding : Building
     {
         //1 = GOLD, 2 = LUMBER, 3 = ROCK, 4 = MEATH
-        public Image goldB;
+        public RawImage goldB;
         public RawImage rockB;
         public RawImage lumberB;
+        public List<RawImage> consB;
         public Text Gold;
         public Text Lumber;
         public Text Rock;
+        private int goldCount = 100;
+        private int LumberCount = 100;
+        private int RockCount = 100;
         private int test = 10;
-        private Boolean t_mouse = false;
-        private List<Image> goldBuild = new List<Image>();
+        private Boolean g_mouse = false;
+        private Boolean r_mouse = false;
+        private Boolean l_mouse = false;
+        private Boolean b_mouse = false;
+        private List<RawImage> goldBuild = new List<RawImage>();
         private List<RawImage> rockBuild = new List<RawImage>();
         private List<RawImage> lumberBuild = new List<RawImage>();
+        private List<RawImage> consBuild = new List<RawImage>();
         Ray ray;
         RaycastHit2D hit;
         private void Start()
-        {/*
+        {
             InvokeRepeating("resourceIncrease", 2.0f, 1.0f);
-            Gold.text = "10";
-            Lumber.text = "10";
-            Rock.text = "10";*/
+            Gold.text = goldCount.ToString();
+            Lumber.text = LumberCount.ToString();
+            Rock.text = RockCount.ToString();
         }
         private void Update()
         {
-            if(t_mouse)
-            goldBuild.ElementAt(goldBuild.Count-1).transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            if (g_mouse)
+            {
+                goldBuild.ElementAt(goldBuild.Count - 1).transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            }
+            if (r_mouse)
+            {
+                rockBuild.ElementAt(rockBuild.Count - 1).transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            }
+            if (l_mouse)
+            {
+                lumberBuild.ElementAt(lumberBuild.Count - 1).transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            }
+            if (b_mouse)
+            {
+                consBuild.ElementAt(consBuild.Count - 1).transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            }
 
             ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
             if (Input.GetMouseButtonDown(0))
             {
-                if (Input.GetMouseButtonDown(0) && t_mouse)
+                //Debug.Log(EventSystem.current.currentSelectedGameObject.ToString());
+                if (Input.GetMouseButtonDown(0) && g_mouse)
                 {
                     goldBuild.ElementAt(goldBuild.Count - 1).transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-                    t_mouse = false;
+                    g_mouse = false;
+                }
+                if (Input.GetMouseButtonDown(0) && r_mouse)
+                {
+                    rockBuild.ElementAt(rockBuild.Count - 1).transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+                    r_mouse = false;
+                }
+                if (Input.GetMouseButtonDown(0) && l_mouse)
+                {
+                    lumberBuild.ElementAt(lumberBuild.Count - 1).transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+                    l_mouse = false;
+                }
+                if (Input.GetMouseButtonDown(0) && b_mouse)
+                {
+                    consBuild.ElementAt(consBuild.Count - 1).transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+                    b_mouse = false;
                 }
             }
 
         }
-        private void OnMouseDown()
-        {
-            
-        }/*
         public void resourceIncrease()
         {
-            test = test + 5;
-            Gold.text = (test*goldBuild.Count).ToString();
-            Lumber.text = test.ToString();
-            Rock.text = test.ToString();
-        }*/
+            goldCount+= 5 * goldBuild.Count;
+            RockCount += 5 * rockBuild.Count;
+            LumberCount += 5 * lumberBuild.Count;
+            Gold.text = goldCount.ToString();
+            Lumber.text = LumberCount.ToString();
+            Rock.text = RockCount.ToString();
+        }
         public void createGoldB()
         {
-            Debug.Log("sa");
-            goldBuild.Add(Instantiate(goldB, new Vector2(0, 0), Quaternion.identity) as Image);
-            goldBuild.ElementAt(goldBuild.Count-1).transform.SetParent(GameObject.Find("Panel").transform, false);
-            t_mouse = true;
+            if (LumberCount - 100 >= 0)
+            {
+                LumberCount -= 100;
+                goldBuild.Add(Instantiate(goldB.GetComponent<RawImage>(), new Vector2(0, 0), Quaternion.identity) as RawImage);
+                goldBuild.ElementAt(goldBuild.Count - 1).transform.SetParent(GameObject.Find("Panel").transform, false);
+                g_mouse = true;
+            }
         }
         public void createRockB()
         {
-            rockBuild.Add(Instantiate(rockB, new Vector2(0, 0), Quaternion.identity) as RawImage);
-            rockBuild.ElementAt(goldBuild.Count - 1).transform.SetParent(GameObject.Find("Panel").transform, false);
-            t_mouse = true;
+            if (goldCount - 100 >= 0)
+            {
+                goldCount -= 100;
+                rockBuild.Add(Instantiate(rockB.GetComponent<RawImage>(), new Vector2(0, 0), Quaternion.identity) as RawImage);
+                rockBuild.ElementAt(rockBuild.Count - 1).transform.SetParent(GameObject.Find("Panel").transform, false);
+                r_mouse = true;
+            }
         }
         public void createLumberB()
         {
-            lumberBuild.Add(Instantiate(lumberB, new Vector2(0, 0), Quaternion.identity) as RawImage);
-            lumberBuild.ElementAt(lumberBuild.Count - 1).transform.SetParent(GameObject.Find("Panel").transform, false);
-            t_mouse = true;
+            if (RockCount - 100 >= 0)
+            {
+                RockCount -= 100;
+                lumberBuild.Add(Instantiate(lumberB.GetComponent<RawImage>(), new Vector2(0, 0), Quaternion.identity) as RawImage);
+                lumberBuild.ElementAt(lumberBuild.Count - 1).transform.SetParent(GameObject.Find("Panel").transform, false);
+                l_mouse = true;
+            }
+        }
+        public void createBuilding()
+        {
+            if (EventSystem.current.currentSelectedGameObject.ToString().Contains("1st"))
+            {
+                consBuild.Add(Instantiate(consB.ElementAt(0).GetComponent<RawImage>(), new Vector2(0, 0), Quaternion.identity) as RawImage);
+                consBuild.ElementAt(consBuild.Count - 1).transform.SetParent(GameObject.Find("Panel").transform, false);
+                b_mouse = true;
+            }
+            if (EventSystem.current.currentSelectedGameObject.ToString().Contains("2nd"))
+            {
+                consBuild.Add(Instantiate(consB.ElementAt(1).GetComponent<RawImage>(), new Vector2(0, 0), Quaternion.identity) as RawImage);
+                consBuild.ElementAt(consBuild.Count - 1).transform.SetParent(GameObject.Find("Panel").transform, false);
+                b_mouse = true;
+            }
+            if (EventSystem.current.currentSelectedGameObject.ToString().Contains("3rd"))
+            {
+                consBuild.Add(Instantiate(consB.ElementAt(2).GetComponent<RawImage>(), new Vector2(0, 0), Quaternion.identity) as RawImage);
+                consBuild.ElementAt(consBuild.Count - 1).transform.SetParent(GameObject.Find("Panel").transform, false);
+                b_mouse = true;
+            }
+            if (EventSystem.current.currentSelectedGameObject.ToString().Contains("7ust"))
+            {
+                consBuild.Add(Instantiate(consB.ElementAt(3).GetComponent<RawImage>(), new Vector2(0, 0), Quaternion.identity) as RawImage);
+                consBuild.ElementAt(consBuild.Count - 1).transform.SetParent(GameObject.Find("Panel").transform, false);
+                b_mouse = true;
+            }
+            if (EventSystem.current.currentSelectedGameObject.ToString().Contains("7alt"))
+            {
+                consBuild.Add(Instantiate(consB.ElementAt(4).GetComponent<RawImage>(), new Vector2(0, 0), Quaternion.identity) as RawImage);
+                consBuild.ElementAt(consBuild.Count - 1).transform.SetParent(GameObject.Find("Panel").transform, false);
+                b_mouse = true;
+            }
+            if (EventSystem.current.currentSelectedGameObject.ToString().Contains("8ust"))
+            {
+                consBuild.Add(Instantiate(consB.ElementAt(5).GetComponent<RawImage>(), new Vector2(0, 0), Quaternion.identity) as RawImage);
+                consBuild.ElementAt(consBuild.Count - 1).transform.SetParent(GameObject.Find("Panel").transform, false);
+                b_mouse = true;
+            }
+            if (EventSystem.current.currentSelectedGameObject.ToString().Contains("8alt"))
+            {
+                consBuild.Add(Instantiate(consB.ElementAt(6).GetComponent<RawImage>(), new Vector2(0, 0), Quaternion.identity) as RawImage);
+                consBuild.ElementAt(consBuild.Count - 1).transform.SetParent(GameObject.Find("Panel").transform, false);
+                b_mouse = true;
+            }
+            if (EventSystem.current.currentSelectedGameObject.ToString().Contains("9ust"))
+            {
+                consBuild.Add(Instantiate(consB.ElementAt(7).GetComponent<RawImage>(), new Vector2(0, 0), Quaternion.identity) as RawImage);
+                consBuild.ElementAt(consBuild.Count - 1).transform.SetParent(GameObject.Find("Panel").transform, false);
+                b_mouse = true;
+            }
+            if (EventSystem.current.currentSelectedGameObject.ToString().Contains("9alt"))
+            {
+                consBuild.Add(Instantiate(consB.ElementAt(8).GetComponent<RawImage>(), new Vector2(0, 0), Quaternion.identity) as RawImage);
+                consBuild.ElementAt(consBuild.Count - 1).transform.SetParent(GameObject.Find("Panel").transform, false);
+                b_mouse = true;
+            }
         }
     }
 
